dc4afdc96c956ff377bd8952aa44105ba9701519
Bruno <bruno.souza.silva@grad.ufsc.br>
2018-06-21 07:36:41 -0300

    Implementação mínima para a classe Projeto.

--- src/Projeto.java
+++ src/Projeto.java
@@ -1,10 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Projeto {
 
 	private String nome;
 	
+	private List<Ocorrencia> ocorrencias;
+	
 	public Projeto(String nome) {
 		this.nome = nome;
+		this.ocorrencias = new ArrayList<Ocorrencia>();
 	}
 
+	public List<Ocorrencia> ocorrencias() { return ocorrencias; }
+	
+	public void adicionarOcorrencia(Ocorrencia o) { ocorrencias.add(o); }
+	
 }
